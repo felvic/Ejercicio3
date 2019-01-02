a48a8a117a7c8028af7a239c694009de2d48639b
felvic <felrivicu@gmail.com>
2019-01-01 22:54:33 -0400

    ejercicio terminado, archivos agregados

---
+++ README.txt
@@ -0,0 +1,2 @@
+EJERCICIO SACADO DE INTERNET
+https://medium.com/laboratoria-how-to/input-y-output-en-javascript-5a02cac6a35b
---
+++ main.js
@@ -0,0 +1,3 @@
+var miEdad = prompt("Ingresa tu edad");
+var laRespuesta = document.getElementById('respuesta');
+laRespuesta.innerHTML = ("Tienes " + miEdad + " años.");
